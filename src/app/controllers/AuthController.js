fa4b88473c0789a06b3318269ea9075255337f28
htphong01 <thanhphonga4.hbt@gmail.com>
2021-12-02 00:13:08 +0700

    add logout feature

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -2,6 +2,11 @@ class AuthController {
   login (req, res, next) {
     res.send('Welcome');
   }
+
+  logout(req, res, next) {
+    req.session.destroy();
+    res.redirect('/');
+  }
 }
 
 module.exports = new AuthController();
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const AuthController = require('../app/controllers/AuthController');
 
 router.get('/', AuthController.login);
+router.get('/login', AuthController.logout)
 
 module.exports = router;
