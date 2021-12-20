b2649ec5f7c7942233d9c97bc363e10b797dff27
htphong01 <thanhphonga4.hbt@gmail.com>
2021-12-21 01:32:05 +0700

    fix css chatting

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,4 +1,14 @@
 const tabLinks = document.querySelectorAll('.tablinks');
+const interactionCloseBtn = document.querySelector('.body-interaction-close-btn');
+const bodyInteraction = document.querySelector('.body-interaction');
+const interactionControlBtn = document.querySelectorAll('.room-interact-control-item');
+const searchPeopleInput = document.querySelector('.interaction-people-search-block > input');
+const interactionTitle = document.querySelector('.body-interaction-title');
+const bodyInteractionItem = document.querySelectorAll('.body-interaction-content');
+const mainSection = document.querySelector('#main-section')
+
+let isBodyInteractionActive = false;
+
 tabLinks.forEach((tabLink, index) => {
   tabLink.addEventListener('click', function () {
     if (index === 0) {
@@ -18,3 +28,107 @@ tabLinks.forEach((tabLink, index) => {
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 document.querySelector('#join-room-input-id').value = params.room || '';
+
+interactionCloseBtn.onclick = closeBodyInteraction.bind(interactionCloseBtn);
+bodyInteraction.style.display = 'none';
+
+function closeBodyInteraction() {
+  mainSection.classList.add('col-md-12');
+  removeInteractControl();
+  bodyInteraction.classList.add('close');
+  isBodyInteractionActive = false;
+  setTimeout(function () {
+    bodyInteraction.style.display = 'none';
+  }, 100)
+}
+
+interactionControlBtn.forEach((interactBtn, index) => {
+  interactBtn.onclick = function () {
+    
+    switch (index) {
+      case 0:
+        renderInteractionBody('details');
+        console.log(index);
+        break;
+      case 1:
+        renderInteractionBody('people');
+        break;
+      case 2:
+        renderInteractionBody('chatting');
+        break;
+      case 3:
+        renderInteractionBody('activity');
+        break;
+    }
+
+    bodyInteractionItem[index].classList.remove('body-interaction-content');
+
+    if (isBodyInteractionActive) {
+      if (this.classList.contains('active')) {
+        closeBodyInteraction();
+        removeInteractControl();
+      } else {
+        isBodyInteractionActive = true;
+        removeInteractControl();
+        this.classList.add('active');
+        mainSection.classList.remove('col-md-12');
+        // render body interaction
+      }
+    } else {
+      isBodyInteractionActive = true;
+      this.classList.add('active');
+      bodyInteraction.classList.remove('close');
+      bodyInteraction.style.display = 'flex';
+      mainSection.classList.remove('col-md-12');
+    }
+  }
+})
+
+function removeInteractControl() {
+  interactionControlBtn.forEach(item => item.classList.remove('active'));
+}
+
+searchPeopleInput.onfocus = function () {
+  this.parentElement.classList.add('focus');
+}
+
+searchPeopleInput.addEventListener('focusout', function () {
+  this.parentElement.classList.remove('focus');
+})
+
+function renderInteractionBody(type) {
+  switch (type) {
+    case 'details': {
+      interactionTitle.innerHTML = 'Meeting details';
+      hideAllInteractionBody();
+
+      break;
+    }
+    case 'people': {
+      interactionTitle.innerHTML = 'People';
+      hideAllInteractionBody();
+      break;
+    }
+    case 'chatting': {
+      interactionTitle.innerHTML = 'Chat';
+      hideAllInteractionBody();
+      break;
+    }
+    case 'activity': {
+      interactionTitle.innerHTML = 'Activity';
+      hideAllInteractionBody();
+      break;
+    }
+  }
+}
+
+function hideAllInteractionBody() {
+  bodyInteractionItem.forEach(item => {
+    if (!item.classList.contains('body-interaction-content')) {
+      item.classList.add('body-interaction-content');
+    }
+  })
+}
+
+const time = (new Date()).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+document.querySelector('.time-web-room').textContent = time;
--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -2,31 +2,31 @@ import helpers from './helpers.js';
 
 window.addEventListener( 'load', () => {
     //When the chat icon is clicked
-    document.querySelector( '#toggle-chat-pane' ).addEventListener( 'click', ( e ) => {
-        let chatElem = document.querySelector( '#chat-pane' );
-        let mainSecElem = document.querySelector( '#main-section' );
+    // document.querySelector( '#toggle-chat-pane' ).addEventListener( 'click', ( e ) => {
+    //     let chatElem = document.querySelector( '#chat-pane' );
+    //     let mainSecElem = document.querySelector( '#main-section' );
 
-        if ( chatElem.classList.contains( 'chat-opened' ) ) {
-            chatElem.setAttribute( 'hidden', true );
-            mainSecElem.classList.remove( 'col-md-9' );
-            mainSecElem.classList.add( 'col-md-12' );
-            chatElem.classList.remove( 'chat-opened' );
-        }
+    //     if ( chatElem.classList.contains( 'chat-opened' ) ) {
+    //         chatElem.setAttribute( 'hidden', true );
+    //         mainSecElem.classList.remove( 'col-md-9' );
+    //         mainSecElem.classList.add( 'col-md-12' );
+    //         chatElem.classList.remove( 'chat-opened' );
+    //     }
 
-        else {
-            chatElem.attributes.removeNamedItem( 'hidden' );
-            mainSecElem.classList.remove( 'col-md-12' );
-            mainSecElem.classList.add( 'col-md-9' );
-            chatElem.classList.add( 'chat-opened' );
-        }
+    //     else {
+    //         chatElem.attributes.removeNamedItem( 'hidden' );
+    //         mainSecElem.classList.remove( 'col-md-12' );
+    //         mainSecElem.classList.add( 'col-md-9' );
+    //         chatElem.classList.add( 'chat-opened' );
+    //     }
 
-        //remove the 'New' badge on chat icon (if any) once chat is opened.
-        setTimeout( () => {
-            if ( document.querySelector( '#chat-pane' ).classList.contains( 'chat-opened' ) ) {
-                helpers.toggleChatNotificationBadge();
-            }
-        }, 300 );
-    } );
+    //     //remove the 'New' badge on chat icon (if any) once chat is opened.
+    //     setTimeout( () => {
+    //         if ( document.querySelector( '#chat-pane' ).classList.contains( 'chat-opened' ) ) {
+    //             helpers.toggleChatNotificationBadge();
+    //         }
+    //     }, 300 );
+    // } );
 
 
     //When the video frame is clicked. This will enable picture-in-picture
